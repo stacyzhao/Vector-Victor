f69ec62d09878e18997d95bda962c3dc8a6f5269
Stacy Zhao <stacyzhao@hotmail.com>
2016-04-07 08:40:06 -0400

    finished vector add

--- linear_algebra.py
+++ linear_algebra.py
@@ -4,20 +4,22 @@ def shape(vector):
     return (len(vector), )
 
 def vector_add(vector1, vector2):
-    if len(a) != len(b):
+    if len(vector1) != len(vector2):
         raise ShapeError
     return [a + b for a, b in zip(vector1, vector2)]
 
 def vector_sub (vector1, vector2):
-    if len(a) != len(b):
+    if len(vector1) != len(vector2):
         raise ShapeError
     return [x - y for x, y in zip(vector1, vector2)]
 
-# def vector_sum(*args):
-#     max_len = max([len(x) for x in args])
+def vector_sum(*args):
+    max_len = max([len(x) for x in args])
+    return [sum([x[y] for x in args]) for y in range(0,max_len)]
+
 
 # def vector_sum (*args):
-#     vector = [sum(x) for arg in zip(args)]
+#     vector = sum([x for arg in zip(args)])
 #     [for x in args, None]
 #     return vector
 # def vector_sum(*args):
@@ -30,15 +32,23 @@ def vector_multiply(vector, scalar):
     return [x * scalar for x in vector]
 
 def dot(vector1, vector2):
-    if len(a) != len(b):
+    if len(vector1) != len(vector2):
         raise ShapeError
-    return sum([a*b for a, b in zip(vector1, vector2)])
+    return sum([a * b for a, b in zip(vector1, vector2)])
+
 
 # def vector_mean(a_args, *b_args):
 #     return [(a + b) / 2 for a, b in zip(a_args, b_args)]
 
 def vector_mean(vector1 ,vector2):
     return [(a + b) / 2 for a, b in zip(vector1, vector2)]
+
+    foo = []
+    for y in range(0,max_len):
+        temp = []
+        for x in args:
+            temp.append(x[y])
+        foo.append(sum(temp))
 
 # def are_equal(a, b):
 #     [for a,b in zip[a,b]
