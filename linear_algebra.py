2949d228583d9b9e089b9444333549278d16553c
Stacy Zhao <stacyzhao@hotmail.com>
2016-04-06 23:10:33 -0400

    attempted on vector_sum and mean

--- linear_algebra.py
+++ linear_algebra.py
@@ -1,50 +1,51 @@
-# vector shape in shape()
-# vector addition in vector_add()
-# vector subtraction in vector_sub()
-# vector multiplication by a scalar in vector_multiply()
-# mean of multiple vectors in vector_mean()
-# dot product in dot()
-# magnitude in magnitude()
 import math
-
-class ShapeError(Exception):
-    pass
 
 def shape(vector):
     return (len(vector), )
 
-def vector_add(a, b):
+def vector_add(vector1, vector2):
     if len(a) != len(b):
         raise ShapeError
-    return [a + b for a, b in zip(a, b)]
+    return [a + b for a, b in zip(vector1, vector2)]
 
-def vector_sub (a, b):
+def vector_sub (vector1, vector2):
     if len(a) != len(b):
         raise ShapeError
-    return [x - y for x, y in zip(a, b)]
+    return [x - y for x, y in zip(vector1, vector2)]
+
+# def vector_sum(*args):
+#     max_len = max([len(x) for x in args])
 
 # def vector_sum (*args):
-#     max_len = max([len(x) for x in args])
 #     vector = [sum(x) for arg in zip(args)]
+#     [for x in args, None]
 #     return vector
-
-def vector_multiply(vector, scalar):
-    return [x * scalar for x in vector]
-
-def dot(a, b):
-    if len(a) != len(b):
-        raise ShapeError
-    return sum([a*b for (a, b) in zip(a, b)])
-
-def vector_mean(vector1 ,vector2):
-    return [(a + b) / 2 for a, b in zip(vector1, vector2)]
-
-def are_equal():
-    pass
-
-def magnitude(vector):
-    return math.sqrt(sum([x**2 for x in vector]))
 # def vector_sum(*args):
     # find max length of args
     # pad smaller lists with 0s
     # sum all lists
+
+
+def vector_multiply(vector, scalar):
+    return [x * scalar for x in vector]
+
+def dot(vector1, vector2):
+    if len(a) != len(b):
+        raise ShapeError
+    return sum([a*b for a, b in zip(vector1, vector2)])
+
+# def vector_mean(a_args, *b_args):
+#     return [(a + b) / 2 for a, b in zip(a_args, b_args)]
+
+def vector_mean(vector1 ,vector2):
+    return [(a + b) / 2 for a, b in zip(vector1, vector2)]
+
+# def are_equal(a, b):
+#     [for a,b in zip[a,b]
+#     pass
+
+def magnitude(vector):
+    return math.sqrt(sum([x**2 for x in vector]))
+
+class ShapeError(Exception):
+    pass
